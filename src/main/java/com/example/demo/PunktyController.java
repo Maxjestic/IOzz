2711df02c6e6d448aa2d97b129bdb9e91cbdcfb0
Mxjestic <m.kafka00@gmail.com>
2023-04-27 22:45:21 +0200

    zadanie4

--- src/main/java/com/example/demo/PunktyController.java
+++ src/main/java/com/example/demo/PunktyController.java
@@ -6,25 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/punkty")
 public class PunktyController {
-    private StudentService service=new StudentService();
+    final private StudentService service=new StudentService();
     @RequestMapping(
             value = "/students",
             method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Student> getUsers(){
         return service.getStudents().asJava();
     }
     @RequestMapping(
             value = "/students",
             method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public Student addUser(@RequestBody NewStudent newStudent){
         return service.addStudent(newStudent);
     }
