dd94397a9be435c476cb6cec8f6c63ed3abe31db
Mxjestic <m.kafka00@gmail.com>
2023-04-27 22:26:52 +0200

    zadanie3

--- src/test/java/com/example/demo/StudentServiceTest.java
+++ src/test/java/com/example/demo/StudentServiceTest.java
@@ -1,23 +1,23 @@
 package com.example.demo;
 
 import io.vavr.collection.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
 
 public class StudentServiceTest {
     @Test
     public void getEmptyList(){
         final StudentService service = new StudentService();
         List<Student> students = service.getStudents();
-        assertTrue(students.isEmpty());
+        Assertions.assertTrue(students.isEmpty());
     }
 
     @Test
     public void addStudent(){
         final StudentService service = new StudentService();
         final Student created = service.addStudent(new NewStudent("Student1", "1-1-1", "IP"));
-        assertNotNull(created);
+        Assertions.assertNotNull(created);
     }
 
     @Test
@@ -25,7 +25,7 @@ public class StudentServiceTest {
         final StudentService service = new StudentService();
         final Student created = service.addStudent(new NewStudent("Student1", "1-2-3", "IP"));
         final List<Student> all = service.getStudents();
-        assertEquals(created.name, all.get(0).name);
+        Assertions.assertEquals(created.name, all.get(0).name);
     }
 
     @Test
@@ -34,7 +34,7 @@ public class StudentServiceTest {
         final Student created1 = service.addStudent(new NewStudent("Student1", "1-2-3", "IP"));
         final Student created2 = service.addStudent(new NewStudent("Student2", "1-2-3", "IP"));
         final List<Student> all = service.getStudents();
-        assertNotEquals(all.get(0).id, all.get(1).id);
-        assertEquals(2, all.size());
+        Assertions.assertNotEquals(created1.id, created2.id);
+        Assertions.assertEquals(2, all.size());
     }
 }
